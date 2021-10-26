10caf75720e36db253ce14ad9cdefd4d78d00ddc
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-26 12:29:44 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('JS OK');
+
+const number_1 = prompt ('inserire numero 1')
+const number_2 = prompt ('inserire numero 2')
+
+if (number_1 > number_2) {
+    console.log(number_1);
+}
+else {
+    console.log(number_2);
+}
